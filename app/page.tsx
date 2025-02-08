fcb8a4a2b8b4362f8ed181f4dcd2f12815e87a9c
Zhicheng Gong <109425to@gmail.com>
2025-02-08 19:47:49 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,91 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Download, ChevronDown } from "lucide-react";
+
+const introPhotos = [
+  "/intro/Screenshot1.png",
+  "/intro/Screenshot2.png",
+  "/intro/Screenshot3.png",
+  "/intro/Screenshot4.png",
+  "/intro/Screenshot5.png",
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [userSystem, setUserSystem] = useState<string>("unknown");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const checkUserSystem = () => {
+      const userAgent = navigator.userAgent;
+
+      if (/iPad|iPhone|iPod/.test(userAgent)) {
+        return "iOS";
+      }
+
+      if (/android/i.test(userAgent)) {
+        return "Android";
+      }
+
+      if (/Windows|Macintosh|Linux/.test(userAgent)) {
+        return "Desktop";
+      }
+
+      return "unknown";
+    };
+
+    setUserSystem(checkUserSystem());
+  }, []);
+
+  return (
+    <>
+      <header className="w-full flex justify-center p-5 fixed top-0 bg-orange z-10">
+        <Image
+          alt="logo"
+          src={"/text_logo/darkblue.svg"}
+          width={60}
+          height={20}
+        />
+      </header>
+      <main className="w-full bg-orange overflow-y-auto">
+        <div className="h-dvh flex flex-col items-center justify-center">
+          <Image alt="app_icon" src={"/app_icon.svg"} width={60} height={60} />
+          <h1 className="text-darkblue text-xl font-bold mt-5">
+            UKHSC 行動會員 APP
+          </h1>
+          <div className="flex gap-2 items-center mt-3">
+            {userSystem === "Desktop" ? (
+              <div className="bg-darkorange text-white m-1 p-2 px-4 rounded-full font-medium">
+                請使用您的手機下載
+              </div>
+            ) : (
+              <button className="bg-darkblue text-white m-1 p-3 px-5 rounded-full font-medium font-sans flex items-center gap-5">
+                <p>
+                  前往 {userSystem === "iOS" ? "App Store" : "Google Play"} 下載
+                </p>
+                <Download size={20} />
+              </button>
+            )}
+          </div>
+          <ChevronDown size={25} className="opacity-50 absolute bottom-5 z-0" />
+        </div>
+        <div className="p-5 flex flex-col justify-center sm:items-center">
+          <div className="border-b border-gray-300 mb-5">
+            <h2 className="text-darkblue text-xl font-bold my-2">介紹</h2>
+          </div>
+          <div className="overflow-y-auto flex items-center gap-7">
+            {introPhotos.map((photo, index) => (
+              <Image
+                key={index}
+                alt="intro_photo"
+                src={photo}
+                width={220}
+                height={50}
+                className="rounded-xl"
+              />
+            ))}
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        darkblue: "var(--darkblue)",
+        orange: "var(--orange)",
+        darkorange: "#A38568",
       },
     },
   },
