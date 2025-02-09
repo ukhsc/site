884bc7249fdba65d38036e454c142d2daa74f2ba
SiongSng <rrt467778@gmail.com>
2025-02-09 17:35:51 +0800

    feat: use next.js redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => [
+    {
+      source: "/legal/terms",
+      destination: "https://hackmd.io/@SiongSng/Sk0DDYNFyl",
+      permanent: false,
+    },
+    {
+      source: "/legal/privacy-policy",
+      destination: "https://hackmd.io/@SiongSng/S1ahaYVY1l",
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
